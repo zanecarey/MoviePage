58d3e034d64a2d0368f762035664365ab27cfc7e
Zane Carey <zanecarey1@gmail.com>
2017-04-14 15:45:31 -0700

    first commit

---
+++ MvcMovie/Migrations/20170413003810_Rating.Designer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MvcMovie.Models;
+
+namespace MvcMovie.Migrations
+{
+    [DbContext(typeof(MvcMovieContext))]
+    [Migration("20170413003810_Rating")]
+    partial class Rating
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MvcMovie.Models.Movie", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<string>("Rating");
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Movie");
+                });
+        }
+    }
+}
